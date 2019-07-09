f71babdb5e78a0e0b262ddf0c0e20a7046771cbf
VIPhujun <1970481517@qq.com>
2019-07-09 17:11:25 +0800

    hujun

---
+++ src/main/java/aaa/microfinance/MicrofinanceApplication.java
@@ -0,0 +1,13 @@
+package aaa.microfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrofinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run (MicrofinanceApplication.class, args);
+    }
+
+}
